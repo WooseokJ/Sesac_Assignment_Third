46393cb991d767e8aca417ce584f2c7177cd835e
wooseokJ <55547933+WooseokJ@users.noreply.github.com>
2022-08-30 09:54:03 +0900

    feat: initialcommit

---
+++ SeSacWek9/SeSacWek9/Lotto/LottoAPIManager.swift
@@ -0,0 +1,18 @@
+//
+//  LottoAPIManager.swift
+//  SeSacWek9
+//
+//  Created by useok on 2022/08/30.
+//
+
+import Foundation
+
+class LottoAPIManager {
+    static func requestLott() {
+        //shared - 단순한,커스텀x,응답클로저,
+        URLSession.shared
+        // defauilt-configuration - shared 설정유사, 커스텀 O(셀룰러연결여부,백그라운드상태에서 음악재생,다운로드등) , 응답클로저+딜리게이트
+        // URLSession.init(configuration: . )
+        
+    }
+}
---
+++ SeSacWek9/SeSacWek9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSacWek9
+//
+//  Created by useok on 2022/08/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
